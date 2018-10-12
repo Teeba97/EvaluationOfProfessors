b0713020bed41f53c6cca5250e09a8bde5c6a777
Teeba97 <saras4353@gmail.com>
2018-10-12 16:32:41 +0300

    admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
+import { EvaluationComponent } from './evaluation/evaluation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
+    EvaluationComponent
    
   ],
   imports: [
