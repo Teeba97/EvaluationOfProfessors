3ee582ca1f8c838480d69f9687c11a1f10225a4c
Ali Faiq Hussein <alixx1iq@gmail.com>
2018-10-26 00:07:11 +0300

    Lecturer get,delete,add

--- src/app/teaching-staff/teaching-staff.component.ts
+++ src/app/teaching-staff/teaching-staff.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LecturersService } from '../services/lecturers.service';
 
 @Component({
   selector: 'app-teaching-staff',
@@ -7,9 +8,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeachingStaffComponent implements OnInit {
 
-  constructor() { }
+  public lecturers = [] ;
 
-  ngOnInit() {
+  lecturer = {
+    name: '',
+    dep_id: 0,
+    st_id: 0
   }
 
+  constructor(public lecturersService: LecturersService) {
+    
+  }
+
+  ngOnInit() {
+    this.getLecturers()
+  }
+
+
+  // get lecturers
+  getLecturers() {
+    this.lecturersService.get()
+      .subscribe ( 
+        data => {
+          this.lecturers = data;
+        }
+      )
+  }
+
+
+  // delete
+  deleteLecturer(item) {
+    console.log(item);
+    this.lecturersService.delete(item)
+      .subscribe ( 
+        data => {
+          if (data["success"]) {
+            this.getLecturers()
+          } else {
+            window.alert("An error to delete this item ...")
+          }
+        }
+      )
+  }
+
+
+  // add
+  addLecturers() {
+    if (this.lecturer.name != '' && this.lecturer.dep_id != 0 && this.lecturer.st_id != 0) {
+      console.log(this.lecturer)
+      this.lecturersService.add( this.lecturer )
+        .subscribe (
+          data => {
+            if (data["success"]) {
+              this.getLecturers()
+            } else {
+              window.alert("An error to delete this item ...")
+            }
+          }
+        )
+
+    } else {
+      window.alert("من فضلك تأكد من انك قد قمت بأختيار القسم و المرحلة و ادخلت اسم الاستاذ بشكل صحيح")
+    }
+  }
+
+  selectDepartment( event ) {
+    this.lecturer.dep_id =  event.target.value
+  }
+
+  selectstage( event ) {
+    this.lecturer.st_id =  event.target.value
+  }
+
+  selectName( event ) {
+    this.lecturer.name =  event.target.value
+  }
 }
