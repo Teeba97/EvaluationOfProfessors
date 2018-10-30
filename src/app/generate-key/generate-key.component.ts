142e2b23ab44eaa8c7a6d4fd61dec4b23d110c8e
Teeba97 <saras4353@gmail.com>
2018-10-30 15:06:46 +0300

    random up

--- src/app/generate-key/generate-key.component.ts
+++ src/app/generate-key/generate-key.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateKeyComponent implements OnInit {
 
-  constructor() { }
+  randomKey =[]
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    this.getRandomNumber(20)
+  }
+
+
+
+ 
+  getRandomNumber(nNumber:number) {
+  
+    this.randomKey=[]
+    var i = 0
+
+    while(i<nNumber){
+    
+      const random = Math.floor(Math.random() * (999999 - 100000)) + 100000;
+      // this.randomKey.push(random)
+      console.log(random)
+    }
+    
   }
 
 }
